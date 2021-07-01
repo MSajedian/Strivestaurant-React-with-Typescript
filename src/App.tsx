7440e1e272e65e542e6f254cf2731b470138bd63
MSajedian <mohammadsajedian@gmail.com>
2021-07-01 15:48:58 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MyNav from "./components/MyNav";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Reservations from "./components/Reservations";
+import Menu from "./components/Menu";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <MyNav title="Strivestaurant" />
+        <Route
+          exact
+          path="/"
+          render={(routerProps) => (
+            <Home {...routerProps} title="Strivestaurant" />
+          )}
+        />
+        <Route path="/reservations" component={Reservations} />
+        <Route path="/menu" component={Menu} />
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
+
+
+// import { RouteComponentProps } from "react-router-dom";
+
+// interface FunctionalComponentProps {}
+
+// interface MyOwnProps {
+//   title: string;
+// }
+// type DetailComponentProps = RouteComponentProps & MyOwnProps;
+
+// const App = (prop: RouteComponentProps) => {
+// const App = (routerProps: RouteComponentProps) => {
+
+// const App: React.FunctionComponent<DetailComponentProps> = () => {
+
+
+// export interface AppProps {}
+
+// const App: React.SFC<AppProps> = () => {
+// const App: React<AppProps> = () => {
+//   return (  );
+// }
+
+// export default App;
---
+++ src/types/interfaces.ts
@@ -0,0 +1,28 @@
+export interface Dish {
+  id: number;
+  name: string;
+  image: string;
+  category: string;
+  label: string;
+  price: string;
+  description: string;
+  comments: Comment[];
+}
+
+export interface Comment {
+  id: number;
+  rating: number;
+  comment: string;
+  author: string;
+  date: string;
+}
+
+export interface Reservation {
+  _id?: string;
+  name: string;
+  phone: string;
+  numberOfPersons: number;
+  smoking: boolean;
+  dateTime: string;
+  specialRequests: string;
+}
